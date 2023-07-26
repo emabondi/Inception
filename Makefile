2dad823d9a7debe44f58fb32f7b62f57ed8b9e2f
emabondi <emabond69@gmail.com>
2023-07-26 17:55:18 +0200

    start docker-compose nginx & mariadb

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
+NAME=inception
 
+all:
+	sudo docker-compose -f srcs/docker-compose.yaml up --build
+
+stop:
+	sudo docker-compose -f srcs/docker-compose.yaml down
 nginx:
 	sudo docker container run --workdir /srcs/requirements/nginx/. --rm -p 443:443 --name cont-nginx test-nginx
 
 mariadb:
 	sudo docker container run --workdir /srcs/requirements/nginx/. --rm --name cont-mariadb test-mariadb
+
+prune:
+	sudo docker prune -a
--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,4 +1,45 @@
+#!/bin/bash
 
-echo    "MariaDB starting..."
+if [ ! -d "/run/mysqld" ]
+then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-exec    /usr/bin/mysqld --user=mysql --console
+if [ ! -d "/var/lib/mysql/wp_database" ]
+then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+	tmp=`mktemp`
+
+	if [ ! -f "$tmp" ]
+	then
+		return 1
+	fi
+
+	cat << EOF > $tmp
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpwd';
+
+CREATE DATABASE wp_database DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE USER 'ebondi'@'%' IDENTIFIED BY 'ebondipwd';
+GRANT ALL PRIVILEGES ON wp_database.* TO 'ebondi'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
+	rm -f $tmp
+fi
+
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s/#bind-address=.*/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
+
+echo "MariaDB starting"
+
+exec /usr/bin/mysqld --user=mysql --console
